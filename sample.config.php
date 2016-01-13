57af4c1505fae92abe82f133d5cd7b94c44531fe
Omer Tashir <omer_tashir@hotmail.com>
2016-01-13 16:03:50 +0200

    updated template files, using dynamic pathes for images

--- sample.config.php
+++ sample.config.php
@@ -10,6 +10,7 @@ define('DIR_APPLICATION', '/var/www/html/sample-commerce/catalog/');
 define('DIR_SYSTEM', '/var/www/html/sample-commerce/system/');
 define('DIR_LANGUAGE', '/var/www/html/sample-commerce/catalog/language/');
 define('DIR_TEMPLATE', '/var/www/html/sample-commerce/catalog/view/theme/');
+define('DIR_THEME_DEFAULT_IMAGE', 'http://localhost/sample-commerce/catalog/view/theme/default/image');
 define('DIR_CONFIG', '/var/www/html/sample-commerce/system/config/');
 define('DIR_IMAGE', '/var/www/html/sample-commerce/image/');
 define('DIR_CACHE', '/var/www/html/sample-commerce/system/storage/cache/');
