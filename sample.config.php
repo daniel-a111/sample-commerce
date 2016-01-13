44fec383f5bd0fb6ec1e53cfa4e82dff80f15e20
Omer Tashir <omer_tashir@hotmail.com>
2016-01-13 15:28:44 +0200

    updated files for init daniel branch

---
+++ sample.config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/sample-commerce/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/sample-commerce/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/sample-commerce/catalog/');
+define('DIR_SYSTEM', '/var/www/html/sample-commerce/system/');
+define('DIR_LANGUAGE', '/var/www/html/sample-commerce/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/sample-commerce/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/sample-commerce/system/config/');
+define('DIR_IMAGE', '/var/www/html/sample-commerce/image/');
+define('DIR_CACHE', '/var/www/html/sample-commerce/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/sample-commerce/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/sample-commerce/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/sample-commerce/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/sample-commerce/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
