bf297d574b76ddeacabd5d1a6db82c85620c83a2
Omer Tashir <omer_tashir@hotmail.com>
2016-01-13 16:29:46 +0200

    fix pathes for images dir & update featured.php to support changes

--- catalog/controller/module/featured.php
+++ catalog/controller/module/featured.php
@@ -28,6 +28,8 @@ class ControllerModuleFeatured extends Controller {
 				$product_info = $this->model_catalog_product->getProduct($product_id);
 
 				if ($product_info) {
+
+
 					if ($product_info['image']) {
 						$image = $this->model_tool_image->resize($product_info['image'], $setting['width'], $setting['height']);
 					} else {
@@ -58,12 +60,15 @@ class ControllerModuleFeatured extends Controller {
 						$rating = false;
 					}
 
+					$best_price = $product_info['best_price'];
+
 					$data['products'][] = array(
 						'product_id'  => $product_info['product_id'],
 						'thumb'       => $image,
 						'name'        => $product_info['name'],
 						'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('config_product_description_length')) . '..',
 						'price'       => $price,
+						'best_price'  => $best_price,
 						'special'     => $special,
 						'tax'         => $tax,
 						'rating'      => $rating,
